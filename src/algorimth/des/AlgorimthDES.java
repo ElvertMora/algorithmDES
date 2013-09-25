a907f2a3f7eed16ebc7ace6c5ad1557c6ff17373
zero <zero@Elvert-THINK>
2013-09-24 22:27:43 -0500

    primer comit algoritmo DES

---
+++ src/algorimth/des/AlgorimthDES.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorimth.des;
+
+/**
+ *
+ * @author zero
+ */
+public class AlgorimthDES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
